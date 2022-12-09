3e5c4e7287dcb3319cf75b3ec56e16b1b1d2d60b
RodriMidas19 <rodri.midas@gmail.com>
2022-12-09 10:54:21 +0000

    Start up API

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyparser.json());
+app.listen(3000,()=>{
+    console.log('listening on port 3000');
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_LAC',
+    port: 3306
+});
+db.connect(err=>{
+    if(err){console.log(err,'dberror');}
+    else{console.log('database connected...');}
+});
+
+//Get data from Direcao
+app.get('/lac/direcao', (req, res)=>{
+    let qry = `SELECT * FROM direcao order by id`;
+
+    db.query(qry, (err,result) => {
+        if(err){
+            console.log(err,'errs');
+        }
+        if(result.length >0){
+            res.send({
+                message:'direcao data get',
+                data: result
+            });
+        }
+        else{
+            console.log('data not found');
+        }
+    })
+})
+
+//Get data from Equipas
+app.get('/lac/equipas',(req, res)=>{
+    let qry = `SELECT * FROM equipas order by id`;
+
+    db.query(qry,(err,result)=>{ 
+        if(err){
+            console.log(err,'erros');
+        }
+        if(result.length >0){
+            res.send({
+                message:'equipas get',
+                data:result
+            })
+        }
+        else{console.log('data not found');}
+    })
+})
